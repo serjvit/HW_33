3a6c0d49095c133f5f894db45b422ca37f426a9d
Serj Vit <serjvit84@gmail.com>
2017-08-24 18:39:20 -0700

    TestNG

---
+++ src/test/java/core/SubtractTest.java
@@ -0,0 +1,18 @@
+package core;
+import org.testng.annotations.*;
+import static org.hamcrest.MatcherAssert.*;
+import static org.hamcrest.Matchers.*;
+public class SubtractTest {
+       @Test
+       public void subtract_test_instanceOf() {
+              assertThat(Calculator.subtract(8, 2), instanceOf(Double.class));}
+       @Test
+       public void subtract_test_2_param() {
+              assertThat(Calculator.subtract(8, 2), equalTo(6.0));}
+       @Test
+       public void subtract_test_3_param() {
+              assertThat(Calculator.subtract(6, 2, 2), equalTo(2.0));}
+       @Test
+       public void subtract_test_4_param() {
+              assertThat(Calculator.subtract(10, 2, 2, 2), equalTo(4.0));}
+}
